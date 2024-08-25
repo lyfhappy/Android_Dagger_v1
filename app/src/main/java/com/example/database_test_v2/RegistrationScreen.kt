1687a44182319ec24855b83119b4e69c653aad07
anubhav <advelestica.design@gmail.com>
2024-08-25 18:24:29 +0530

    Revert "V2 -Student reg"
    This reverts commit 8e3f67b4e1a755b335a8b92640c18c3c7e952f14.

--- app/src/main/java/com/example/database_test_v2/RegistrationScreen.kt
+++
@@ -1,4 +0,0 @@
-package com.example.database_test_v2
-
-class RegistrationScreen {
-}
--- app/src/main/java/com/example/database_test_v2/Screen.kt
+++
@@ -1,6 +0,0 @@
-package com.example.database_test_v2
-
-sealed class Screen(val route: String) {
-    object HomeScreen:Screen("home_screen")
-    object RegistrationScreen:Screen("registration_screen")
-}
